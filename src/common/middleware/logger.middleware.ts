6847723b63c9728332273af6468968fc9446f509
motty93 <rdwbocungelt5@gmail.com>
2021-06-13 17:20:41 +0900

    refactor: middlewareのディレクトリ変更

--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -1,8 +1,9 @@
 import { Injectable, NestMiddleware } from '@nestjs/common'
+import { Request, Response, NextFunction } from 'express'
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
+  use(req: Request, res: Response, next: NextFunction) {
     console.log('Request')
     next()
   }
