7c0b1dea4a19cee6c9944fcfe930e8ebc7189c53
motty93 <rdwbocungelt5@gmail.com>
2021-06-15 22:04:20 +0900

    feat: product schemaとinterface定義

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common'
-import { ProductsController } from './products.controller';
-import { ProductsService } from './products.service';
+import { MongooseModule } from '@nestjs/mongoose'
+import { ProductsController } from './products.controller'
+import { ProductsService } from './products.service'
+import { ProductSchema } from './products.model'
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forFeature([{ name: 'Product', schema: ProductSchema }]),
+  ],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
