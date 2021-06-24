62612ad16b29e5220214a8afd9f2db2e432c923e
motty93 <rdwbocungelt5@gmail.com>
2021-06-24 22:18:52 +0900

    feat: OpenAPI swagger導入

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 // import { LoggerMiddleware } from './common/middleware/logger.middleware'
 
@@ -6,6 +7,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   // グローバルにmiddleware登録
   // app.use(LoggerMiddleware)
+  const options = new DocumentBuilder()
+    .setTitle('Swagger example')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('test')
+    .build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('swagger', app, document)
+
   await app.listen(process.env.PORT || 3000)
 }
 bootstrap()
